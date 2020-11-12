a6cbda5535b8255b2a7fdffca2e6ed68a5f52a53
yinxuan <yinxuansh@126.com>
2020-11-12 13:15:27 +0800

    尝试部署1.0

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // 基本路径
     /* 部署生产环境和开发环境下的URL：可对当前环境进行区分，baseUrl 从 Vue CLI 3.3 起已弃用，要使用publicPath */
     /* baseUrl: process.env.NODE_ENV === 'production' ? './' : '/' */
-    publicPath: process.env.NODE_ENV === "production" ? "/resume" : "./",
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
     // 输出文件目录
     outputDir: "dist",
     // eslint-loader 是否在保存的时候检查
