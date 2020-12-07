565ff5e4bf297726c01340e2565be0f50747beca
yinxuan <yinxuansh@126.com>
2020-12-07 16:16:03 +0800

    尝试部署2.0

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,7 @@ const routes = [
 
 const router = createRouter({
   base:'/dist',
-  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
